a49c11216d075c7833559baef4df746b63b8b68d
ShagunPandey2 <162408000+ShagunPandey2@users.noreply.github.com>
2024-03-12 10:06:19 +0530

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,16 +1,18 @@
+let details=[]
 function solve() { 
+    let name=getElementById('first').value;
     let password =  
         document.getElementById('password').value; 
     let repassword =  
         document.getElementById('repassword').value; 
     let mobile =  
         document.getElementById('mobile').value; 
-    let mail = 
+    let maill = 
         document.getElementById('email').value; 
     let flag = 1; 
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-  
-    if (!emailRegex.test(mail)) { 
+    
+    if (!emailRegex.test(maill)) { 
         flag = 0; 
         pass.innerText =  
             'Please enter a valid email address.'; 
@@ -18,7 +20,7 @@ function solve() {
             pass.innerText = ""; 
         }, 3000); 
     } 
-  
+    
     if (password !== repassword) { 
         flag = 0; 
         pass.innerText = 
@@ -43,4 +45,30 @@ function solve() {
     } 
     if (flag) 
         alert("Form submitted"); 
+    let detail={
+        name:name,
+        mail:maill
+        
+
+    };
+        details.push(detail);
+        alert("details");
+        console.log(details)
+}
+function getdetails(){
+    const mail=document.getElementById('mail').value
+    let detail=null;
+    for(let i=0;i<details.length;i++)
+    {
+        if(student[i]['mail']===mail){
+            detail=details[i];
+            break;
+        }
+    }
+    if(detail){
+        alert("HEY RIGHT");
+    }
+    else{
+        alert('oops!');
+    }
 }
